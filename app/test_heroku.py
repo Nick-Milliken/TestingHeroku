baf78e555f9e39e8f92f15b8694fd0498eb705bc
Nick Milliken <nimi9113@colorado.edu>
2021-05-15 21:37:49 -0600

    using pickle file instead of raw code

--- app/test_heroku.py
+++ app/test_heroku.py
@@ -3,32 +3,33 @@ app = Flask(__name__)
 
 data = pd.read_csv("https://github.com/Nick-Milliken/deploy-ml/raw/main/ramen/ramen-ratings.csv")
 
-X = data.Stars
+#X = data.Stars
 
-X = X.replace('Unrated', np.NaN)
-X = pd.to_numeric(X)
-X = np.nan_to_num(X)
-X = X.reshape(-1, 1)
+#X = X.replace('Unrated', np.NaN)
+#X = pd.to_numeric(X)
+#X = np.nan_to_num(X)
+#X = X.reshape(-1, 1)
 
-y = data.Country
+#y = data.Country
 
-X, y = X, y
-X_train, X_test, y_train, y_test = train_test_split(X, y,
+#X, y = X, y
+#X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                     random_state=0)
-pipe = Pipeline([('scaler', StandardScaler()), ('lr', LogisticRegression())])
+#pipe = Pipeline([('scaler', StandardScaler()), ('lr', LogisticRegression())])
 # The pipeline can be used as any other estimator
 # and avoids leaking the test set into the train set
-pipe.fit(X_train, y_train)
-Pipeline(steps=[('scaler', StandardScaler()), ('lr', LogisticRegression())])
-pipe.score(X_test, y_test)
+#pipe.fit(X_train, y_train)
+#Pipeline(steps=[('scaler', StandardScaler()), ('lr', LogisticRegression())])
+#pipe.score(X_test, y_test)
 
-lr = LogisticRegression(random_state = 42)
-lr.fit = lr.fit(X_train, y_train)
-lr.fit = lr.predict(X)
-lr.proba = lr.predict_proba(X)
+#lr = LogisticRegression(random_state = 42)
+#lr.fit = lr.fit(X_train, y_train)
+#lr.fit = lr.predict(X)
+#lr.proba = lr.predict_proba(X)
 #print(lr.fit)
 #print(lr.proba)
-
+#with open('model.py', 'rb') as g:
+ #    pickle.load(g)
 
 @app.route('/')
 def homeview():
@@ -36,6 +37,7 @@ def homeview():
 
 @app.route('/table')
 def table():
-    model = lr.proba
-    model = tabulate(model, tablefmt='html')
-    return model
+    with open('model.py', 'rb') as g:
+	model = pickle.load(g)
+	model = model.head()
+	return model
